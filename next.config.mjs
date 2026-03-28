3ad10ade600a3d1afc499754da45bd2c96aea6e3
hafnas008 <hafnas008@gmail.com>
2026-03-28 23:40:37 +0300

    Add env vars to next.config and fix tailwind version

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://cjpvrtnrdkwxtonoeihp.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqcHZydG5yZGt3eHRvbm9laWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5OTMxNzMsImV4cCI6MjA1ODU2OTE3M30.EpRPe-4Ch4DWnSqIz8uUmm8TGR__DNwqpRB8eWuV0IM',
+    NEXT_PUBLIC_NOTIFY_URL: 'https://n8n.srv1279727.hstgr.cloud/webhook/notify',
+    NEXT_PUBLIC_DEFAULT_PHONE: '97455534024',
+  }
+}
 export default nextConfig
